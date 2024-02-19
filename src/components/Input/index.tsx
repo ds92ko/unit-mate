6c2f09a9b930f632890933a728f5950e3f92f98b
rlacodud <74367207+rlacodud@users.noreply.github.com>
2024-02-19 22:09:01 +0900

    feat: input autoFocus 추가

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,13 +1,21 @@
+import { useEffect, useRef } from 'react';
 import { InputContents } from './index.css';
 
 function Input({ ...props }) {
+  const inputElement = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputElement.current) {
+      inputElement.current.focus();
+    }
+  }, []);
   return (
     <input
       className={InputContents}
       placeholder="계산할 숫자 쉼표 혹은 스페이스로 구분하여 입력"
       type="text"
       id="insertNumber"
-      readOnly
+      ref={inputElement}
       {...props}
     />
   );
